bce963c5e6b0cde226ba92ced601b27aaae6c233
Jacob Lange <jacob@withpersona.com>
2022-03-16 09:23:42 -0700

    chore: update package name for v2

--- Package.swift
+++ Package.swift
@@ -2,15 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "PersonaPhoneNumberKit",
+    name: "PersonaPhoneNumberKit2",
     platforms: [
         .iOS(.v11), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
-        .library(name: "PersonaPhoneNumberKit", targets: ["PersonaPhoneNumberKit"])
+        .library(name: "PersonaPhoneNumberKit2", targets: ["PersonaPhoneNumberKit2"])
     ],
     targets: [
-        .target(name: "PersonaPhoneNumberKit",
+        .target(name: "PersonaPhoneNumberKit2",
                 path: "PhoneNumberKit",
                 exclude: ["Resources/Original",
                           "Resources/README.md",
